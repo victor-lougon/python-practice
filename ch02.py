b84f4e2071d751e271db4f8b054d9906e3c041a5
Victor Lougon <vlougon@gmail.com>
2026-04-21 13:43:38 -0300

    Add chapter 2 exercises

---
+++ ch02.py
@@ -0,0 +1,41 @@
+#Exercise 2.1 & 2.2:
+message = "Hi everyone!"
+print(message)
+
+#Exercise 2.3:
+name = "Victor"
+print("Hello " + name + ", would you like to learn some Python today?")
+
+#Exercise 2.4:
+name = "peter"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#Exercise 2.5:
+print('William James once said, "No decision is, in itself, a decision."')
+
+#Exercise 2.6
+famous_person = "William James"
+message = famous_person + ' once said, "No decision is, in itself, a decision."'
+print(message)
+
+#Exercise 2.7
+
+person_name = "  Victor Lougon "
+print("\n\t" + person_name)
+print(person_name)
+print(person_name.rstrip())
+print(person_name.lstrip())
+print(person_name.strip())
+
+#Exercise 2.8
+
+print(2+6)
+print(2*4)
+print(10-2)
+print(4/0.5)
+print(2**3)
+
+favorite_number = 6
+print("My favorite number is " + str(favorite_number) + ".")
